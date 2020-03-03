18c534b4745158ad3a34ff2e5fff14a216de8065
Vasiliy <saimonises@gmail.com>
2020-03-03 23:36:45 +0300

    Подготовка к storybook

--- info-pj/src/App.js
+++ info-pj/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Button as AntBtn} from 'antd';
 
 function App() {
-  return (
-    <div className="App">
-app
-    </div>
-  );
+    return (
+        <div className="App">
+          <AntBtn type="primary">Ant Design</AntBtn>
+        </div>
+    );
 }
 
 export default App;
